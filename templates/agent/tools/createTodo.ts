2da91d8b23ca1728057bb65f0d9e94fdcc531735
Sarim Malik <sarim@rubriclab.com>
2023-10-26 14:33:38 -0400

    Remove drizzle ORM

--- templates/agent/tools/createTodo.ts
+++ templates/agent/tools/createTodo.ts
@@ -1,26 +1,17 @@
-import { DynamicStructuredTool } from 'langchain/tools'
+import {DynamicStructuredTool} from 'langchain/tools'
 import z from 'zod'
-import { db, todos } from "../schema";
 
 export async function createTodo({text}: {text: string}) {
-  
-    const addedTodo = await db.insert(todos).values([{
-        complete: 0,
-        text
-    }])
-  
-    console.log(addedTodo)
-      
-    return JSON.stringify(addedTodo)
+	return JSON.stringify(text)
 }
 
 export default function createTodoTool() {
-  return new DynamicStructuredTool({
-    description: "Create a todo",
-    func: async ({text}) => {
-      return JSON.stringify(await createTodo({text}));
-    },
-    name: "listTodos",
-    schema: z.object({text: z.string()})
-  });
+	return new DynamicStructuredTool({
+		description: 'Create a todo',
+		func: async ({text}) => {
+			return JSON.stringify(await createTodo({text}))
+		},
+		name: 'listTodos',
+		schema: z.object({text: z.string()})
+	})
 }
--- templates/agent/tools/listTodos.ts
+++ templates/agent/tools/listTodos.ts
@@ -1,22 +1,17 @@
-import { DynamicStructuredTool } from 'langchain/tools'
+import {DynamicStructuredTool} from 'langchain/tools'
 import z from 'zod'
-import { db, todos } from "../schema";
 
 export async function listTodos() {
-  const allTodos = await db.select().from(todos).all()
-
-  console.log(allTodos)
-  
-  return allTodos;
+	return []
 }
 
 export default function listTodosTool() {
-  return new DynamicStructuredTool({
-    description: "List all todos",
-    func: async () => {
-      return JSON.stringify(await listTodos());
-    },
-    name: "listTodos",
-    schema: z.object({})
-  });
+	return new DynamicStructuredTool({
+		description: 'List all todos',
+		func: async () => {
+			return JSON.stringify(await listTodos())
+		},
+		name: 'listTodos',
+		schema: z.object({})
+	})
 }
