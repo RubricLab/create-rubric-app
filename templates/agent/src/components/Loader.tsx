1395414cd9df5064c7b7adacbb29944381ddc909
tedspare <ted.spare@gmail.com>
2023-11-01 11:03:51 -0400

    Fix SVG prop warning

--- templates/agent/src/components/Loader.tsx
+++ templates/agent/src/components/Loader.tsx
@@ -11,11 +11,13 @@ export default function Loader() {
 				cy='12'
 				r='10'
 				stroke='currentColor'
-				stroke-width='4'></circle>
+				strokeWidth='4'
+			/>
 			<path
 				className='opacity-75'
 				fill='currentColor'
-				d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'></path>
+				d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
+			/>
 		</svg>
 	)
 }
