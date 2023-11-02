2831cf4340b202714465fe94d83b2116b6a5b7fb
tedspare <ted.spare@gmail.com>
2023-11-02 17:16:38 -0400

    Clean up agent route code

--- templates/agent/src/app/api/agent/route.ts
+++ templates/agent/src/app/api/agent/route.ts
@@ -1,7 +1,9 @@
-import basicAgent from '~/agents/basic'
+import {basicAgent} from '~/agents/basic'
 
 export async function POST(req) {
-	const {input} = await req.json()
+	const { input } = await req.json()
+	
 	const stream = basicAgent({input})
+	
 	return new Response(await stream)
 }
