0a96d38219e917e1fd1119dee3cdf3364b53e461
tedspare <ted.spare@gmail.com>
2023-11-02 17:02:08 -0400

    Pass className to loader

--- templates/agent/src/components/Loader.tsx
+++ templates/agent/src/components/Loader.tsx
@@ -1,7 +1,7 @@
-export default function Loader() {
+export default function Loader({className}: {className?: string}) {
 	return (
 		<svg
-			className='h-6 w-6 animate-spin text-stone-400'
+			className={`${className} animate-spin`}
 			xmlns='http://www.w3.org/2000/svg'
 			fill='none'
 			viewBox='0 0 24 24'>
