1199433f372df625bea71ba35cd04ea8c51243d5
tedspare <ted.spare@gmail.com>
2023-11-02 16:37:22 -0400

    Widen navbar

--- templates/agent/src/components/Nav.tsx
+++ templates/agent/src/components/Nav.tsx
@@ -2,18 +2,17 @@ import Link from 'next/link'
 
 export default function Nav({title}: {title: string}) {
 	return (
-		<div className='fixed top-0 z-20 flex w-full flex-col p-5'>
+		<div className='fixed top-0 z-20 flex w-full justify-between px-5 pt-3'>
 			<Link
 				className='text-xl font-bold no-underline'
 				href='https://github.com/rubriclab/create-rubric-app'
 				target='_blank'>
 				{title}
 			</Link>
-
 			<Link
 				href='https://rubriclabs.com/blog/cra'
 				target='_blank'
-				className='no-underline'>
+				className='text-secondary no-underline'>
 				learn more
 			</Link>
 		</div>
