67ea7516ee296ede0f52754e391edd398b9b7555
tedspare <ted.spare@gmail.com>
2023-11-02 16:47:30 -0400

    Center nav items

--- templates/agent/src/components/Nav.tsx
+++ templates/agent/src/components/Nav.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function Nav({title}: {title: string}) {
 	return (
-		<div className='fixed top-0 z-20 flex w-full justify-between px-5 pt-3'>
+		<div className='fixed top-0 z-20 flex w-full items-center justify-between px-5 pt-3'>
 			<Link
 				className='text-xl font-bold no-underline'
 				href='https://github.com/rubriclab/create-rubric-app'
