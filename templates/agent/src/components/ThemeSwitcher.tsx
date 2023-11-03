fcc18f0ea1ddc7370616957e1796a6fa3cdf1949
shanvy <138781964+iamshanvy@users.noreply.github.com>
2023-11-04 03:29:55 +0800

    Only render theme toggle on client (#8)
    * fix: hydration errors
    
    ---------
    
    Co-authored-by: Ted Spare <ted.spare@gmail.com>

--- templates/agent/src/components/ThemeSwitcher.tsx
+++ templates/agent/src/components/ThemeSwitcher.tsx
@@ -1,10 +1,18 @@
 'use client'
-
 import {Moon, Sun} from 'lucide-react'
 import {useTheme} from 'next-themes'
+import {useEffect, useState} from 'react'
 
 export function ThemeSwitcher() {
 	const {theme, setTheme} = useTheme()
+
+	const [mounted, setMounted] = useState(false)
+
+	useEffect(() => {
+		setMounted(true)
+	}, [])
+
+	if (!mounted) return null
 
 	return (
 		<div>
