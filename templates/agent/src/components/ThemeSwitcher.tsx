a7e4477c2ed23e2741fe81e556d8473258c41357
shanvy <iamshanvy@gmail.com>
2023-11-03 21:47:05 +0800

    fix: use lucide imports

--- templates/agent/src/components/ThemeSwitcher.tsx
+++ templates/agent/src/components/ThemeSwitcher.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import {Moon, Sun} from 'lucide-react'
 import {useTheme} from 'next-themes'
 import {useEffect, useState} from 'react'
 
@@ -19,47 +20,7 @@ export function ThemeSwitcher() {
 			<button
 				className='bg-primary'
 				onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>
-				{theme === 'light' ? (
-					<svg
-						xmlns='http://www.w3.org/2000/svg'
-						width='24'
-						height='24'
-						viewBox='0 0 24 24'
-						fill='none'
-						stroke='currentColor'
-						stroke-width='2'
-						stroke-linecap='round'
-						stroke-linejoin='round'
-						className='lucide lucide-moon'>
-						<path d='M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z' />
-					</svg>
-				) : (
-					<svg
-						xmlns='http://www.w3.org/2000/svg'
-						width='24'
-						height='24'
-						viewBox='0 0 24 24'
-						fill='none'
-						stroke='currentColor'
-						stroke-width='2'
-						stroke-linecap='round'
-						stroke-linejoin='round'
-						className='lucide lucide-sun'>
-						<circle
-							cx='12'
-							cy='12'
-							r='4'
-						/>
-						<path d='M12 2v2' />
-						<path d='M12 20v2' />
-						<path d='m4.93 4.93 1.41 1.41' />
-						<path d='m17.66 17.66 1.41 1.41' />
-						<path d='M2 12h2' />
-						<path d='M20 12h2' />
-						<path d='m6.34 17.66-1.41 1.41' />
-						<path d='m19.07 4.93-1.41 1.41' />
-					</svg>
-				)}
+				{theme === 'light' ? <Moon /> : <Sun />}
 			</button>
 		</div>
 	)
