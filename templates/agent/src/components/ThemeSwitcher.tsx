c295cf3054ca40f22c5a128bea496c93eedec23d
shanvy <138781964+iamshanvy@users.noreply.github.com>
2023-11-04 01:05:07 +0800

    Let there be light (or dark) (#5)
    * feat: let there be light (or dark)

---
+++ templates/agent/src/components/ThemeSwitcher.tsx
@@ -0,0 +1,17 @@
+'use client'
+import {Moon, Sun} from 'lucide-react'
+import {useTheme} from 'next-themes'
+
+export function ThemeSwitcher() {
+	const {theme, setTheme} = useTheme()
+
+	return (
+		<div>
+			<button
+				className='bg-primary'
+				onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>
+				{theme === 'light' ? <Moon /> : <Sun />}
+			</button>
+		</div>
+	)
+}
