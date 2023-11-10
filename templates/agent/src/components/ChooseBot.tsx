aecfab90ca87925d325ea2facc204b736f4c23bd
arihanv <arihanvaranasi@gmail.com>
2023-11-09 22:23:46 -0600

    style: Add styling to the dropdown

--- templates/agent/src/components/ChooseBot.tsx
+++ templates/agent/src/components/ChooseBot.tsx
@@ -1,3 +1,5 @@
+import {ChevronDown} from 'lucide-react'
+
 type Props = {
 	bot: string
 	setBot: (bot: string) => void
@@ -7,7 +9,7 @@ const models = ['gpt-3.5-turbo', 'gpt-4-1106-preview', 'gpt-4', 'gpt-4-32k']
 
 export default function ChooseBot({bot, setBot}: Props) {
 	return (
-		<div className='flex w-full justify-start'>
+		<div className='relative flex w-full justify-start font-mono'>
 			<select
 				value={bot}
 				onChange={e => setBot(e.currentTarget.value)}>
@@ -21,6 +23,9 @@ export default function ChooseBot({bot, setBot}: Props) {
 					)
 				})}
 			</select>
+			<div className='absolute right-0 flex h-full items-center pr-4'>
+				<ChevronDown />
+			</div>
 		</div>
 	)
 }
