6d321dad3d7db28aa7d8f7a1bc2d7cabb92a66ac
arihanv <arihanvaranasi@gmail.com>
2023-11-11 00:16:01 -0600

    fix: Only log model name in dev

--- templates/agent/src/app/api/agent/route.ts
+++ templates/agent/src/app/api/agent/route.ts
@@ -2,7 +2,7 @@ import {basicAgent} from '~/agents/basic'
 
 export async function POST(req) {
 	const { input, modelName } = await req.json()
-	console.log("Using OpenAI", modelName)
+	if(process.env.NODE_ENV === 'development') console.log("Using OpenAI", modelName)
 	
 	const stream = basicAgent({input, modelName})
 	
