1df22adc801535127d9b4c3671a09ad56c3654f7
Sarim Malik <sarim@rubriclabs.com>
2024-02-28 16:01:03 -0500

    Simplify API route, add license to package.json

--- templates/blank/src/app/api/hello/route.ts
+++ templates/blank/src/app/api/hello/route.ts
@@ -1,10 +1,5 @@
-import {basicAgent} from '~/agents/basic'
-
-export async function POST(req) {
-	const { input, modelName } = await req.json()
-	if(process.env.NODE_ENV === 'development') console.log("Using OpenAI", modelName)
-	
-	const stream = basicAgent({input, modelName})
-	
-	return new Response(await stream)
+export async function GET(request: Request) {
+	return new Response('Hello from create-rubric-app!', {
+		status: 200
+	})
 }
