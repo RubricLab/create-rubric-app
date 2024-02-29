34a63149e0587438abd87ea91620312eaffc6359
Ted Spare <ted.spare@gmail.com>
2024-02-29 09:42:28 -0500

    Make page async function

--- templates/blank/src/app/page.tsx
+++ templates/blank/src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import DemoForm from '~/ui/home/demo-form'
 
-export default function Page() {
+export default async function Page() {
 	return (
 		<div className='flex h-screen w-full flex-col justify-center gap-10 p-5 sm:p-20'>
 			<h1>Welcome</h1>
