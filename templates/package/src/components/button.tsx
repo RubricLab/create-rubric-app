847249f41cfc9550818bc86768977284979aa4ee
DexterStorey <dexter@dexterstorey.com>
2024-06-25 13:29:09 -0700

    init package template

---
+++ templates/package/src/components/button.tsx
@@ -0,0 +1,5 @@
+'use client'
+
+export const Button = () => {
+	return <button>Click me</button>
+}
---
+++ templates/package/src/index.ts
@@ -0,0 +1,5 @@
+import './styles.css'
+
+export * from './components/button'
+
+export * from './utils/sum'
